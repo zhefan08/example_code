4d75fa00f934b1768a26a5764dfe4d8ebffb2678
Zhe Fan <zhefan08@gmail.com>
2015-01-26 01:26:28 -0800

    java example

---
+++ java_example/src/com/zfan/Main.java
@@ -0,0 +1,11 @@
+package com.zfan;
+
+/**
+ * Created by zfan on 1/26/15.
+ */
+public class Main {
+    public static int main(String[] input) {
+        System.out.println("Hello Intellj and Java");
+        return 0;
+    }
+}
